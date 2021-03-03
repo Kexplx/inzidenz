62b84cbfab47245b1cc340d335c196b69de672f2
Oscar Rosner <oscar.rosner@web.de>
2021-03-04 00:06:11 +0100

    replace faq modal with faw drawer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import { Button, Divider, Spin } from 'antd';
-import Modal from 'antd/lib/modal/Modal';
+import { Button, Divider, Drawer, Row, Spin } from 'antd';
 import Text from 'antd/lib/typography/Text';
 import { useEffect, useState } from 'react';
 import CountyCard from './CountyCard';
+import { CloseOutlined } from '@ant-design/icons';
 import { getGermanDateFormat } from './date-helpers';
 
 let countyCodes = [9362, 9562, 9162, 9564, 9179, 9372, 9248, 9278];
@@ -98,7 +98,7 @@ function App() {
       <h1>COVID-19 | 7-Tage-Inzidenz</h1>
       {counties.length ? (
         <>
-          <Divider plain>
+          <Divider orientation="left" plain>
             <Text type="secondary">
               Stand: {getGermanDateFormat(new Date(counties[0].lastUpdated))}
             </Text>
@@ -111,6 +111,11 @@ function App() {
               county={c}
             />
           ))}
+          <Divider orientation="left">
+            <Button onClick={() => setIsFaqOpen(true)} type="link">
+              FAQ
+            </Button>
+          </Divider>
         </>
       ) : (
         <div className="spinner-container">
@@ -118,38 +123,52 @@ function App() {
         </div>
       )}
 
-      <Divider>
-        <Button onClick={() => setIsFaqOpen(true)} type="link">
-          FAQ
-        </Button>
-      </Divider>
-
-      <Modal visible={isFaqOpen} onCancel={() => setIsFaqOpen(false)} footer={''} title="FAQ">
-        <div style={{ textAlign: 'justify' }}>
-          <h4>Was ist die 7-Tage-Inzidenz?</h4>
-          Die 7-Tage-Inzidenz ist eine wichtige Grundlage für die Einschätzung der Entwicklung der
-          Corona-Pandemie. Der Wert bildet die COVID Fälle pro 100.000 Einwohner*innen in den
-          letzten 7 Tagen ab.
+      <Drawer
+        placement="bottom"
+        height="100%"
+        closable={false}
+        visible={isFaqOpen}
+        onClose={() => setIsFaqOpen(false)}
+      >
+        <div className="container">
+          <h1>COVID-19 | 7-Tage-Inzidenz</h1>
+          <Row justify="space-between">
+            <h2>FAQ</h2>
+            <Button
+              onClick={() => setIsFaqOpen(false)}
+              type="text"
+              aria-label="Close"
+              shape="circle"
+            >
+              <CloseOutlined />
+            </Button>
+          </Row>
+          <div style={{ textAlign: 'justify' }}>
+            <h4>Was ist die 7-Tage-Inzidenz?</h4>
+            Die 7-Tage-Inzidenz ist eine wichtige Grundlage für die Einschätzung der Entwicklung der
+            Corona-Pandemie. Der Wert bildet die COVID Fälle pro 100.000 Einwohner*innen in den
+            letzten 7 Tagen ab.
+          </div>
+          <div style={{ textAlign: 'justify' }} className="mt-2">
+            <h4>Woher stammen die Daten?</h4>
+            Diese Anwendung verwendet eine API von{' '}
+            <a href="https://public.opendatasoft.com/explore/dataset/covid-19-germany-landkreise/api/">
+              opendatasoft
+            </a>
+            . Opendatasoft bezieht die Daten von dem offiziellen Datenhub des Robert-Koch-Instituts.
+          </div>
+          <div style={{ textAlign: 'justify' }} className="mt-2">
+            <h4>Wo sind die anderen Landkreise?</h4>
+            Sie können jeden Landkreis in Deutschland betrachten. Tragen Sie dazu den
+            Gemeindeschlüssel des jeweiligen Landkreises in der URL ein und laden Sie die Seite neu.
+            <br />
+            <br />
+            Mit der URL <Text code>https://kexplx.github.io/inzidenz?q=6412,5112</Text> werden bspw.
+            die Daten für Frankfurt (6412) und Duisburg (5112) geladen. Beachten Sie die Trennung
+            der Gemeindeschlüssel mit einem Komma!
+          </div>
         </div>
-        <div style={{ textAlign: 'justify' }} className="mt-2">
-          <h4>Woher stammen die Daten?</h4>
-          Diese Anwendung verwendet eine API von{' '}
-          <a href="https://public.opendatasoft.com/explore/dataset/covid-19-germany-landkreise/api/">
-            opendatasoft
-          </a>
-          . Opendatasoft bezieht die Daten von dem offiziellen Datenhub des Robert-Koch-Instituts.
-        </div>
-        <div style={{ textAlign: 'justify' }} className="mt-2">
-          <h4>Wo sind die anderen Landkreise?</h4>
-          Sie können jeden Landkreis in Deutschland betrachten. Tragen Sie dazu den
-          Gemeindeschlüssel des jeweiligen Landkreises in der URL ein und laden Sie die Seite neu.
-          <br />
-          <br />
-          Mit der URL <Text code>https://kexplx.github.io/inzidenz?q=6412,5112</Text> werden bspw.
-          die Daten für Frankfurt (6412) und Duisburg (5112) geladen. Beachten Sie die Trennung der
-          Gemeindeschlüssel mit einem Komma!
-        </div>
-      </Modal>
+      </Drawer>
     </div>
   );
 }
