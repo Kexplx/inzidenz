fc5c7a1b9d9cc883c31a4c73a67af24b183c7f8a
Oscar Rosner <oscar.rosner@web.de>
2021-04-23 19:02:18 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
-import { Button, Row, Spin } from 'antd';
-import { LineChartOutlined, LoadingOutlined, ReloadOutlined } from '@ant-design/icons';
+import { Button, Drawer, Row, Spin } from 'antd';
+import {
+  AlertOutlined,
+  HomeOutlined,
+  LineChartOutlined,
+  LoadingOutlined,
+  ReloadOutlined,
+} from '@ant-design/icons';
 import { useCounties } from './useCounties';
 import { useGermany } from './useGermany';
 import GermanyDescription from './GermanyDescription';
@@ -7,10 +13,13 @@ import AlertDate from './AlertDate';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import CountyTable from './CountyTable';
 import Chart from './Chart';
+import { useState } from 'react';
+import Text from 'antd/lib/typography/Text';
 
 function App() {
   const [counties, reloadCounties] = useCounties();
   const [germany, reloadGermany] = useGermany();
+  const [showDrawer, setShowDrawer] = useState(false);
 
   return (
     <div className="container">
@@ -22,6 +31,15 @@ function App() {
                 Historie
               </Button>
             </Link>
+            <Button
+              className="ml-1"
+              onClick={() => setShowDrawer(!showDrawer)}
+              type="dashed"
+              size="small"
+              icon={<AlertOutlined />}
+            >
+              Notbremse
+            </Button>
           </div>
           <Row
             className="mt-1"
@@ -69,6 +87,64 @@ function App() {
           ) : (
             <CountyTable counties={counties} />
           )}
+
+          <Drawer
+            style={{ margin: 0, padding: '-10px' }}
+            placement="bottom"
+            height="100%"
+            closable={false}
+            onClose={() => setShowDrawer(false)}
+            visible={showDrawer}
+          >
+            <div style={{ textAlign: 'justify' }} className="container mt-2">
+              <Button
+                onClick={() => setShowDrawer(false)}
+                type="primary"
+                icon={<HomeOutlined />}
+                size="small"
+              >
+                Übersicht
+              </Button>
+              <h2 className="mt-2">§ 28b Infektionsschutzgesetz</h2>
+              <h4>
+                Überschreitet in einem Landkreis oder einer kreisfreien Stadt an drei aufeinander
+                folgenden Tagen die 7-Tage-Inzidenz den Schwellenwert von 100, gelten die folgenden
+                Maßnahmen.
+              </h4>
+              <ul>
+                <li>
+                  <Text strong>Ausgangsbeschränkungen: </Text>Ausgangsbeschränkung von 22 bis 5 Uhr
+                  (Spaziergänge und Joggen alleine bleiben bis Mitternacht erlaubt).{' '}
+                </li>
+
+                <li>
+                  <Text strong>Kontaktbeschränkungen: </Text>Im privaten und öffentlichen Raum darf
+                  sich ein Haushalt mit höchstens einer weiteren Person treffen, wobei Kinder bis 14
+                  Jahre ausgenommen sind.
+                </li>
+
+                <li>
+                  <Text strong>Schulen und Kitas: </Text>Ab einer Sieben-Tage-Inzidenz von 165
+                  sollen Schulen schließen und auf Distanzunterricht umstellen, bei dem die Schüler
+                  mittels digitaler Lösungen zu Hause lernen. Kindertagesstätten müssen ab einer
+                  7-Tage-Inzidenz von 165 in den Notbetrieb umschalten.
+                </li>
+
+                <li>
+                  <Text strong>Einkaufen: </Text> Läden dürfen nur für Kunden öffnen, die einen
+                  negativen Corona-Test vorlegen und einen Termin gebucht haben. Viele Geschäfte
+                  müssen außerdem ab einer Inzidenz von 150 dichtmachen und dürfen dann nur noch das
+                  Abholen bestellter Waren anbieten. Ausgenommen sind unter anderem Supermärkte,
+                  Drogerien und Apotheken umschalten.
+                </li>
+
+                <li>
+                  <Text strong>Kultur: </Text> Schließen müssen gemäß Notbremse Theater, Opern,
+                  Konzerthäuser, Museen, Ausstellungen und Gedenkstätten.
+                </li>
+              </ul>
+            </div>
+          </Drawer>
         </Route>
         <Route path="/history" component={Chart} />
       </Router>
--- src/Chart.js
+++ src/Chart.js
@@ -1,24 +1,17 @@
-import {
-  HomeOutlined,
-  LineChartOutlined,
-  LoadingOutlined,
-  WarningOutlined,
-} from '@ant-design/icons';
-import { Button, Drawer, Row, Select, Spin } from 'antd';
+import { HomeOutlined, LoadingOutlined } from '@ant-design/icons';
+import { Button, Row, Select, Spin } from 'antd';
 import Text from 'antd/lib/typography/Text';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { XAxis, ResponsiveContainer, LineChart, Line, YAxis, ReferenceDot } from 'recharts';
-import { addDecimalPoint, getCountyName } from './helpers';
+import { addDecimalPoint } from './helpers';
 
 const germanyHistoryUrl = 'https://valid-alpha-268602.ew.r.appspot.com//germany-history';
 const countiesHistoryUrl = 'https://valid-alpha-268602.ew.r.appspot.com//counties';
 
 const Chart = () => {
   const [countiesHistory, setCountiesHistory] = useState(null);
-
-  const [showDrawer, setShowDrawer] = useState(false);
   const [countiesChartData, setCountiesChartData] = useState(null);
   const [germanyChartData, setGermanyChartData] = useState(null);
   const [showInzidenz, setShowInzidenz] = useState(false);
@@ -52,18 +45,6 @@ const Chart = () => {
       inzidenz: c.inzidenz.toFixed(0),
       lastUpdated: c.lastUpdated.replace('.2021, 00:00 Uhr', ''),
     }));
-  };
-
-  const notbremseApplies = history => {
-    if (
-      history[history.length - 1].inzidenz >= 100 &&
-      history[history.length - 2].inzidenz >= 100 &&
-      history[history.length - 3].inzidenz >= 100
-    ) {
-      return true;
-    }
-
-    return false;
   };
 
   return (
@@ -136,17 +117,6 @@ const Chart = () => {
         <Row align="middle" justify="space-between">
           <h4 className="m-0">
             Städte & Landkreise <Text type="secondary">(Inzidenz)</Text>
-            {notbremseApplies(countiesChartData) && (
-              <Button
-                size="small"
-                style={{ display: 'block' }}
-                onClick={() => setShowDrawer(true)}
-                icon={<WarningOutlined />}
-                type="dashed"
-              >
-                Notbremse
-              </Button>
-            )}
           </h4>
           <Select size="small" onChange={handleCountySelect} defaultValue={'9362'}>
             {Object.entries(countiesHistory).map(([a, b]) => (
@@ -188,93 +158,6 @@ const Chart = () => {
           <Spin indicator={<LoadingOutlined />} tip="Lade Historie" />
         </Row>
       )}
-      <Drawer
-        style={{ margin: 0, padding: '-10px' }}
-        placement="bottom"
-        height="100%"
-        closable={false}
-        onClose={() => setShowDrawer(false)}
-        visible={showDrawer}
-      >
-        {countiesChartData && (
-          <div style={{ textAlign: 'justify' }} className="container mt-2">
-            <Button
-              onClick={() => setShowDrawer(false)}
-              type="primary"
-              size="small"
-              icon={<LineChartOutlined />}
-            >
-              Historie
-            </Button>
-            <h2 className="mt-2">§ 28b Infektionsschutzgesetz</h2>
-            <h4>
-              Überschreitet in einem Landkreis oder einer kreisfreien Stadt an drei aufeinander
-              folgenden Tagen die 7-Tage-Inzidenz den Schwellenwert von 100, gelten die folgenden
-              Maßnahmen (
-              {notbremseApplies(countiesChartData) ? (
-                <Text type="danger">Gilt in {getCountyName(countiesChartData[0])}</Text>
-              ) : (
-                <Text type="success">Gilt nicht in {getCountyName(countiesChartData[0])}</Text>
-              )}
-              ).
-            </h4>
-            <ul>
-              <li>
-                Private Zusammenkünfte im öffentlichen oder privaten Raum sind nur noch zulässig,
-                wenn an ihnen höchstens die Angehörigen eines Haushalts und eine weitere Person
-                einschließlich der zu ihrem Haushalt gehörenden Kinder bis zur Vollendung des 14.
-                Lebensjahres teilnehmen.
-              </li>
-              <li>
-                Es gilt eine nächtliche Ausgangsbeschränkung in der Zeit von 21 Uhr bis 5 Uhr. Das
-                Verlassen der Wohnung ist nur noch bei Vorliegen triftiger Gründe erlaubt.
-                Kindertagesstätten und Schulen sind ab einer Inzidenz von 200 an drei
-                aufeinanderfolgenden Tagen für den Präsenzbetrieb geschlossen. Ausnahmen sind
-                insbesondere für Abschlussklassen vorgesehen.
-              </li>
-              <li>
-                Der Betrieb von Wettannahmestellen, Museen, Galerien, zoologischen und botanischen
-                Gärten sowie Gedenkstätten für den Publikumsverkehr bleibt insgesamt untersagt.
-              </li>
-
-              <li>
-                Sport ist nur zulässig in Form von kontaktloser Ausübung von Individualsportarten,
-                die allein, zu zweit oder mit den Angehörigen des eigenen Haushalts ausgeübt werden
-                sowie bei Ausübung von Individual- und Mannschaftssportarten im Rahmen des
-                Wettkampf- und Trainingsbetriebs des Spitzen- und Profisports.
-              </li>
-
-              <li>
-                Körpernahe Dienstleistungen wie Kosmetik-, Nagel-, Massage-, Tattoo- und
-                Piercingstudios sowie von kosmetischen Fußpflegeeinrichtungen und ähnlichen
-                Einrichtungen sind mit Ausnahme von medizinisch notwendigen Behandlungen
-                (insbesondere Physio- und Ergotherapie, Logopädie, Podologie und Fußpflege)
-                geschlossen. Auch Sonnenstudios sind zu schließen.
-              </li>
-              <li>
-                Für Kundinnen und Kunden von Friseurbetrieben und Barbershops ist ein vorheriger
-                Schnelltest erforderlich.
-              </li>
-              <li>
-                Der Betrieb von Musik-, Kunst- und Jugendkunstschulen ist nur im Rahmen des
-                Onlineunterrichts zulässig.
-              </li>
-              <li>
-                Ladengeschäfte dürfen keine Abholangebote mehr anbieten. Es sind nur noch
-                Lieferdienste zulässig.
-              </li>
-              <li>
-                Soweit Ladengeschäfte der Grundversorgung, also insbesondere aus dem
-                Lebensmittelbereich, geöffnet bleiben, wird die Begrenzung der maximal zulässigen
-                Verkaufsfläche pro Kundin oder Kunde nochmals verschärft von 10 auf 20 Quadratmeter
-                (bei Ladenflächen bis 800 Quadratmeter) und von 20 auf 40 Quadratmeter (für die über
-                800 Quadratmeter hinausgehenden Flächen).
-              </li>
-              <li>Baumärkte sind geschlossen.</li>
-            </ul>
-          </div>
-        )}
-      </Drawer>
     </>
   );
 };
